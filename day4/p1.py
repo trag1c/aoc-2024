2a13efdcb69713e3964877e5c5bb3b5bf43dbe90
trag1c <trag1cdev@yahoo.com>
2024-12-04 23:15:21 +0100

    day4: p1 (270 -> 258 bytes)

--- day4/p1.py
+++ day4/p1.py
@@ -1,4 +1,4 @@
-L=open(p:=0).readlines();D=[(1,1),(1,0),(1,O:=-1),(0,1),(0,O),(O,1),(O,0),(O,O)]
+*L,=open(p:=0);D=(1,1),(1,0),(1,O:=-1),(0,1),(0,O),(O,1),(O,0),(O,O)
 def f(s,X,Y,x,y):
  if max(z:=not s,0>x,0>y):return z
  try:return(0,f(s[1:],X,Y,x+X,y+Y))[L[y][x]==s[0]]
